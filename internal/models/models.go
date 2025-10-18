e8a10b35b16b4e31687451c0c7ab12f49f6eb856
Dario Camonita <danterolle@parrotsec.org>
2025-10-18 12:09:05 +0200

    feat: add truncation flags to CommandResult model

--- internal/models/models.go
+++ internal/models/models.go
@@ -8,6 +8,8 @@ type CommandResult struct {
 	TimedOut        bool   `json:"timed_out"`
 	PartialResults  bool   `json:"partial_results"`
 	ExecutionTimeMs *int64 `json:"execution_time_ms,omitempty"` // Added for C++ executor
+	StdoutTruncated bool   `json:"stdout_truncated"`
+	StderrTruncated bool   `json:"stderr_truncated"`
 }
 
 type GenericCommandRequest struct {
